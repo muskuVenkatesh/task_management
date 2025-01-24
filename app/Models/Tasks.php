b427ffefac7d1da05c308c1d62a3a5af2c9f0509
MuskuVenkatesh <venkateshmusku6@gmail.com>
2025-01-24 17:22:50 +0530

    Initial commit for Laravel project

---
+++ app/Models/Tasks.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Tasks extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['title', 'description', 'project_id', 'assigned_to', 'status'];
+}
---
+++ database/migrations/2025_01_24_103351_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->foreignId('project_id')->constrained('projects')->onDelete('cascade');
+            $table->foreignId('assigned_to')->constrained('users')->onDelete('set null');
+            $table->enum('status', ['Pending', 'In Progress', 'Completed']);
+            $table->timestamps();
+            $table->softDeletes(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
